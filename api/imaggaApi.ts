eaa0f0b74c5456448ed77caa9df5f29bc35c7e60
Oleg Chibikov <ochibikov@gmail.com>
2023-04-27 14:17:52 +1000

    Basic working POC of fetching data

---
+++ api/imaggaApi.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import getEnvVars from '../environment';
+import { ImagePickerAsset } from 'expo-image-picker';
+
+interface Tag {
+  confidence: number;
+  tag: {
+    en: string;
+  };
+}
+
+async function uploadImageAndGetTags(image: ImagePickerAsset): Promise<Tag[]> {
+  // const { imaggaKey, imaggaSecret } = getEnvVars();
+  // const apiKey = imaggaKey;
+  // const apiSecret = imaggaSecret;
+
+  // const formData = new FormData();
+  // formData.append('image_base64', image.base64!);
+
+  // const response = await axios.post('https://api.imagga.com/v2/tags', formData, {
+  //   auth: {
+  //     username: apiKey,
+  //     password: apiSecret,
+  //   },
+  //   headers: {
+  //     'Content-Type': 'multipart/form-data',
+  //   },
+  // });
+
+  // return response.data.result.tags;
+  return new Array(50).fill({
+    confidence: 1,
+    tag:{
+    en: 'stub',
+  },
+});
+}
+
+export {uploadImageAndGetTags}
--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -1,15 +1,65 @@
 import React, { FunctionComponent, useState } from 'react';
-import { Button, Image, ScrollView, StyleSheet, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { ImagePickerAsset } from 'expo-image-picker';
+import getEnvVars from '../environment';
+import { uploadImageAndGetTags } from '../api/imaggaApi';
 
-const ImageUploader: FunctionComponent = ()=> {
+interface ImageSelectorProps {
+  images: ImagePickerAsset[];
+  onImagesSelected: (images: ImagePickerAsset[]) => void;
+}
+
+const ImageSelector: FunctionComponent<ImageSelectorProps> = ({ images, onImagesSelected }) => {
+  const [selectedImages, setSelectedImages] = useState<ImagePickerAsset[]>([]);
+
+  function toggleImageSelection(image: ImagePickerAsset) {
+    if (selectedImages.includes(image)) {
+      setSelectedImages(selectedImages.filter((img) => img !== image));
+    } else {
+      setSelectedImages([...selectedImages, image]);
+    }
+  }
+
+  return (
+    <ScrollView contentContainerStyle={styles.imageContainer}>
+      {images.map((image, index) => (
+        <TouchableOpacity key={index} onPress={() => toggleImageSelection(image)}>
+          <Image source={{ uri: image.uri }} style={styles.image} />
+          {selectedImages.includes(image) && <Text style={styles.checkmark}>✓</Text>}
+        </TouchableOpacity>
+      ))}
+      <Button title="Done" onPress={() => onImagesSelected(selectedImages)} />
+    </ScrollView>
+  );
+};
+
+interface Tag {
+  confidence: number;
+  tag: {
+    en: string;
+  };
+}
+
+async function uploadImages(images: ImagePickerAsset[]): Promise<Tag[][]> {
+  const responses = await Promise.all(
+    images.map(uploadImageAndGetTags)
+  );
+
+  const data = await Promise.all(responses.map((response) => response));
+  return data.map((item) => item);
+}
+
+const ImageUploader: FunctionComponent = () => {
   const [images, setImages] = useState<ImagePickerAsset[]>([]);
+  const [selectedImages, setSelectedImages] = useState<ImagePickerAsset[]>([]);
+  const [tags, setTags] = useState<Tag[][]>([]);
 
   async function pickImages() {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsMultipleSelection: true,
+      base64: true
     });
 
     if (!result.canceled) {
@@ -17,17 +67,26 @@ const ImageUploader: FunctionComponent = ()=> {
     }
   }
 
+  async function getTags() {
+    const tags = await uploadImages(selectedImages);
+    setTags(tags);
+  }
+
   return (
     <View style={styles.container}>
       <Button title="Select Images" onPress={pickImages} />
-      <ScrollView contentContainerStyle={styles.imageContainer}>
-        {images.map((image, index) => (
-          <Image key={index} source={{ uri: image.uri }} style={styles.image} />
-        ))}
-      </ScrollView>
+      <ImageSelector images={images} onImagesSelected={(a)=>{setSelectedImages(a); getTags()}} />
+      {tags.map((imageTags, index) => (
+        <View key={index}>
+          <Text>Image {index + 1}:</Text>
+          {imageTags.map((tag, index) => (
+            <Text key={index}>{tag.tag.en}</Text>
+          ))}
+        </View>
+      ))}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -44,6 +103,17 @@ const styles = StyleSheet.create({
     height: 100,
     margin: 4,
   },
+  checkmark: {
+    position: 'absolute',
+    top: 4,
+    right: 4,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    width: 24,
+    height: 24,
+    textAlign: 'center',
+    lineHeight: 24,
+  },
 });
 
-export {ImageUploader}
+export { ImageUploader };
