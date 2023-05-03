dc5a37cedaee4e9cafd45e58cb7b6bbfe6d62b27
Oleg Chibikov <ochibikov@gmail.com>
2023-05-03 14:32:25 +1000

    Better tag handling

--- helpers/tagHelper.ts
+++ helpers/tagHelper.ts
@@ -1,30 +1,33 @@
-function getUniqueTags(
-  tags: Tag[][],
-  extraTags: string[],
-  isAi: boolean
-): string[] {
-  // Extract unique tags from the 'en' field
-  const uniqueTags = Array.from(
-    new Set(tags.flatMap((tags) => tags.map((tag) => tag.tag.en)))
-  );
-  const maxTags = 49;
-  const updatedTags = [...extraTags, ...uniqueTags].slice(
-    0,
-    isAi ? maxTags - 3 : maxTags
-  ); // not more than 50 images
-  if (isAi) {
-    updatedTags.push('Generative AI');
-    updatedTags.push('Generative');
-    updatedTags.push('AI');
-  }
-  return updatedTags;
-}
+const generativeAiTags = ['Generative AI', 'Generative', 'AI'];
+const maxTags = 49;
 
 interface Tag {
   confidence: number;
   tag: {
     en: string;
   };
+}
+
+function getUniqueTags(
+  tags: Tag[][],
+  previousTags: string[],
+  isAi: boolean
+): string[] {
+  const flatTags = tags.flatMap((tags) => tags.map((tag) => tag.tag.en));
+  let uniqueTags: Set<string>;
+  if (previousTags.length) {
+    uniqueTags = new Set<string>(previousTags);
+    generativeAiTags.forEach((tag) => uniqueTags.delete(tag));
+    flatTags.forEach((tag) => uniqueTags.add(tag));
+  } else {
+    uniqueTags = new Set(flatTags);
+  }
+  const updatedTags = [...uniqueTags].slice(
+    0,
+    isAi ? maxTags - generativeAiTags.length : maxTags
+  ); // not more than maxTags tags
+
+  return isAi ? updatedTags.concat(generativeAiTags) : updatedTags;
 }
 
 export type { Tag };
